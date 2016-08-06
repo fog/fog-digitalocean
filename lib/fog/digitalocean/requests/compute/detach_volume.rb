992bc0dfb8ae1f893ee7ceda24b788c5670c4eba
Suraj Shirvankar <surajshirvankar@gmail.com>
2016-08-06 21:12:44 +0200

    Detach/attach by name

--- lib/fog/digitalocean/requests/compute/detach_volume.rb
+++ lib/fog/digitalocean/requests/compute/detach_volume.rb
@@ -13,7 +13,22 @@ module Fog
               'Content-Type' => "application/json; charset=UTF-8",
             },
             :method  => 'POST',
-            :path    => "v2/images/#{volume_id}/actions",
+            :path    => "v2/volumes/#{volume_id}/actions",
+            :body    => encoded_body,
+          )
+        end
+        def detach_volume_by_name(volume_name, droplet_id, region)
+          body = { :type => 'detach', volume_name: volume_name, droplet_id: droplet_id, region: region}
+
+          encoded_body = Fog::JSON.encode(body)
+
+          request(
+            :expects => [201],
+            :headers => {
+              'Content-Type' => "application/json; charset=UTF-8",
+            },
+            :method  => 'POST',
+            :path    => "v2/volumes/actions",
             :body    => encoded_body,
           )
         end
@@ -60,6 +75,46 @@ module Fog
 
 					response
 				end
+        def detach_volume_by_name(volume_name, droplet_id, region)
+          response         = Excon::Response.new
+          response.status  = 201
+					response.body    = {
+						"action" => {
+							"id" => 68212773,
+							"status" => "in-progress",
+							"type" => "detach_volume",
+							"started_at" => "2015-10-15T17:46:15Z",
+							"completed_at" => null,
+							"resource_id" => null,
+							"resource_type" => "backend",
+							"region" => {
+								"name" => "New York 1",
+								"slug" => "nyc1",
+								"sizes" => [
+									"512mb",
+									"1gb",
+									"2gb",
+									"4gb",
+									"8gb",
+									"16gb",
+									"32gb",
+									"48gb",
+									"64gb"
+								],
+								"features" => [
+									"private_networking",
+									"backups",
+									"ipv6",
+									"metadata"
+								],
+								"available" => true
+							},
+							"region_slug" => "nyc1"
+						}
+					}
+
+					response
+        end
 			end
 		end
 	end
